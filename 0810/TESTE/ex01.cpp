38b5c803fd056b6ab72a0e0dad3f1b3b1da76c07
Sr. Patinho <83667953+martimcabral@users.noreply.github.com>
2024-05-29 10:06:40 +0000

    Update ex01.cpp

--- 0810/TESTE/ex01.cpp
+++ 0810/TESTE/ex01.cpp
@@ -1,134 +1,133 @@
 #include <iostream>
-#include <string>
 #include <limits>
+#include <string>
 #define MAX 100
+
 using namespace std;
 
-/*1. [20] Escreva um programa para simular uma agenda de telefones.
-A estrutura deve conter:
-- Nome
-- E-mail
-- Endereço (contendo os campos para a rua, número, cidade e país)
-- Telefone
-- Data de Aniversário
+struct DataDePublicacao {
+    int dia, mes, ano;
 
-a. [3] Definir a estrutura acima.
-b. [3] Declarar a variável agenda (array) com capacidade de agendar até 100 nomes.
-c. [3] Imprimir os dados da Agenda.
-d. [5] Pesquisar na agenda pelo o nome da pessoa.
-e. [3] Imprimir os dados de todas as pessoas que fazem anos nesse mês
-f. [3] O programa deve ter um menu principal oferecendo as opções acima.*/
-
-struct Empresa {
-    int next_contract = 0, temp_pri = 0;
-    string agenda[MAX] = {}, telefone[MAX] = {}, email[MAX] = {}, temp_sec = "";
-    string endereco_rua[MAX] = {}, endereco_numero[MAX] = {}, endereco_cidade[MAX] = {}, endereco_pais[MAX] = {};
-    string data_aniversario_dia[MAX] = {}, data_aniversario_mes[MAX] = {}, data_aniversario_ano[MAX] = {};
-
-    void adicionar() {
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "\n\n\nInsira o Nome: ";
-        getline(cin, agenda[next_contract]);
-        cout << "\nInsira o E-mail: ";
-        getline(cin, email[next_contract]);
-        cout << "\nInsira o Endereco [RUA]: ";
-        getline(cin, endereco_rua[next_contract]);
-        cout << "\nInsira o Endereco [NUMERO]: ";
-        getline(cin, endereco_numero[next_contract]);
-        cout << "\nInsira o Endereco [CIDADE]: ";
-        getline(cin, endereco_cidade[next_contract]);
-        cout << "\nInsira o Endereco [PAIS]: ";
-        getline(cin, endereco_pais[next_contract]);
-        cout << "\nInsira o Telefone: ";
-        getline(cin, telefone[next_contract]);
-        cout << "\nInsira a Data de Aniversario [DIA]: ";
-        getline(cin, data_aniversario_dia[next_contract]);
-        cout << "\nInsira a Data de Aniversario [MES]: ";
-        getline(cin, data_aniversario_mes[next_contract]);
-        cout << "\nInsira a Data de Aniversario [ANO]: ";
-        getline(cin, data_aniversario_ano[next_contract]);
-        cout << "\n\n### Telefone adiconado com sucesso! ###\n\n";
-        next_contract++;
-    };
-
-    void listar_por_nome() {
-        cout << "Quem deseja encontrar? [NOME EXATO]: ";
-        cin >> temp_sec;
-   
-        for (int i = -1; i <= MAX; i++) {
-            if (temp_sec == agenda[i]) {
-                cout << "\n\n######################";
-                cout << "\nNome: " << agenda[i];
-                cout << "\nE-Mail: " << email[i];
-                cout << "\nEndereco: " << endereco_rua[i] << ", " << endereco_numero[i] << ", " << endereco_cidade[i] << ", " << endereco_pais[i];
-                cout << "\nTelefone: " << telefone[i];
-                cout << "\nData de Aniversario: " << data_aniversario_dia[i] << "/" << data_aniversario_mes[i] << "/" << data_aniversario_ano[i];
-                cout << "\n######################\n\n";
-            }
-        }
-    };
-
-    void listar_por_mes_aniversario() {
-        cout << "Quem deseja encontrar? [MES EXATO]: ";
-        cin >> temp_sec;
-
-        for (int i = -1; i <= MAX; i++) {
-            if (temp_sec == data_aniversario_mes[i]) {
-                cout << "\n\n######################";
-                cout << "\nNome: " << agenda[i];
-                cout << "\nE-Mail: " << email[i];
-                cout << "\nEndereco: " << endereco_rua[i] << ", " << endereco_numero[i] << ", " << endereco_cidade[i] << ", " << endereco_pais[i];
-                cout << "\nTelefone: " << telefone[i];
-                cout << "\nData de Aniversario: " << data_aniversario_dia[i] << "/" << data_aniversario_mes[i] << "/" << data_aniversario_ano[i];
-                cout << "\n######################\n\n";
-            }
-        }
+    void imprimir() {
+        cout << "\nData de Publicacao: ";
+        cout << "\nDia: " << dia << "\nMes: " << mes << "\nAno: " << ano << endl;
     }
-
-    void listar_todos() {
-            for (int i = 0; i <= MAX; i++) {
-                if (agenda[i] == "") {
-                    cout << "";
-                } else {
-                cout << "\n######################";
-                cout << "\nNome: " << agenda[i];
-                cout << "\nEmail: " << email[i];
-                cout << "\nEndereco: " << endereco_rua[i] << ", " << endereco_numero[i] << ", " << endereco_cidade[i] << ", " << endereco_pais[i];
-                cout << "\nTelefone: " << telefone[i];
-                cout << "\nData de Aniversario: " << data_aniversario_dia[i] << "/" << data_aniversario_mes[i] << "/" << data_aniversario_ano[i];
-                cout << "\n######################\n\n";
-            }
-        }
-    };
 };
 
-    int main() {
-        Empresa Empresas;
-        int opcao = -1;
+struct Biblioteca {
+    DataDePublicacao datadepublicacao;
+    string tituloLivro, autorLivro;
 
-        do {
-            cout << "1 - Adicionar Telefone\n";
-            cout << "2 - Listar um Telefone pelo Nome\n";
-            cout << "3 - Listar todos os Telefones pelo Mes\n";
-            cout << "4 - Listar todos os Telefones\n";
-            cout << "0 - Sair\n\n";
-            cout << "Opcao: ";
-            cin >> opcao;
+    void imprimir() {
+        cout << "\nTitulo: " << tituloLivro << "\nAutor: " << autorLivro;
+        datadepublicacao.imprimir();
+    }
+};
 
-            switch (opcao) {
+void imprimirLivros(Biblioteca livro[MAX], int total) {
+    for (int i = 0; i < total; i++) {
+        livro[i].imprimir();
+        cout << "\n\n";
+    }
+};
+
+void inserirLivro(Biblioteca (*livro)[MAX], int (*total)) {
+    Biblioteca biblioteca;
+    
+    cout << "\nInsira um titulo: ";
+    cin >> biblioteca.tituloLivro;
+
+    cout << "\nInsira o autor: ";
+    cin >> biblioteca.autorLivro;
+    
+    cout << "\nInsira a Data de Publicacao: ";
+    cout << "\nDia: ";
+    cin >> biblioteca.datadepublicacao.dia;
+
+    cout << "\nMes: ";
+    cin >> biblioteca.datadepublicacao.mes;
+
+    cout << "\nAno: ";
+    cin >> biblioteca.datadepublicacao.ano;
+
+    (*livro[*total]) = biblioteca;
+    (*total)++;
+};
+
+void encontrarPorTitulo(Biblioteca livro[MAX], int total) {
+    string tituloLivro;
+    cout << "Insira o titulo do Livro que queira encontrar: \n";
+    cin >> tituloLivro;
+
+    bool encontrou = false;
+
+    for (int i = 0; i < total; i++) {
+        if (livro[i].tituloLivro == tituloLivro) {
+            livro[i].imprimir();
+            encontrou = true;
+        }
+    }
+
+    if (encontrou == false) {
+        cout << "\nNao foi possivel encontrar!\n";
+    }
+};
+
+void encontrarPorAno(Biblioteca livro[MAX], int total) {
+    int ano;
+
+    cout << "\nInsira o Ano: ";
+    cin >> ano;
+    
+    bool encontrou = false;
+
+    for (int i = 0; i < total; i++) {
+        if (livro[i].datadepublicacao.ano == ano) {
+            livro[i].imprimir();
+            encontrou = true;
+        }
+    }
+
+    if (encontrou == false) {
+        cout << "\nNao foi possivel encontrar!\n";
+    }
+}
+
+int main() {
+    Biblioteca livro[MAX];
+    int totalLivros = 0;
+    int opcao = 999;
+
+    do {
+        cout << "\n-----------[{- Menu -}]-----------";
+        cout << "\n1 - Inserir um Livro";
+        cout << "\n2 - Imprimir Biblioteca";
+        cout << "\n3 - Pesquisar um Livro pelo Titulo";
+        cout << "\n4 - Pesquisar um Livro pelo Ano";
+        cout << "\n0 - Sair";
+        cout << "\nOpcao: ";
+
+        while (!(cin >> opcao)) {
+            cout << "Opcao invalida! Insira novamente: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        switch (opcao) {
             case 1:
-                Empresas.adicionar();
+                inserirLivro(&livro, &totalLivros);
                 break;
             case 2:
-                Empresas.listar_por_nome();
+                imprimirLivros(livro, totalLivros);
                 break;
             case 3:
-                Empresas.listar_por_mes_aniversario();
+                encontrarPorTitulo(livro, totalLivros);
                 break;
             case 4:
-                Empresas.listar_todos();
+                encontrarPorAno(livro, totalLivros);
                 break;
-            }
-        } while (opcao != 0);
-        return 0;
-    }
+            default:
+                cout << "Essa opcao nao existe!" << endl;
+        }
+    } while (opcao != 0);
+}
